11f41e1472f12df2f9430911da4fbdb417ea4cb4
WIRIN <diaz.zidane@gmail.com>
2020-01-20 12:54:23 +0530

    new linked list

---
+++ hackerrank.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+using namespace std;
+
+
+class Node{
+
+
+public:
+
+	int data;
+	Node* next;
+
+};
+
+
+int main(){
+
+ 
+    Node* head=NULL;
+    Node* second=NULL;
+    Node* third=NULL;
+
+
+    head = new Node;
+    second= new Node;
+    third= new Node;
+
+
+    head->data=1;
+    head->next=second;
+
+    second->data=2;
+    second->next=third;
+
+    third->data=3;
+    third->next=NULL;
+
+
+    while(head!=NULL){
+
+
+    	cout<<head->data;
+    	cout<<"\n";
+
+    	head=head->next;
+    }
+
+}
